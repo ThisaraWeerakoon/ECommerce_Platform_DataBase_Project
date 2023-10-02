8ccce33f2686234daf7e467de771e7bbd861a06f
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2023-10-02 10:07:54 +0530

    deleted

--- client/src/pages/AuthenticanPage.js
+++ client/src/pages/AuthenticanPage.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import SignIn from '../pageComponents/AuthenticationPageComponent/SignIn'
 import AuthenticanPageHeader from '../pageComponents/AuthenticationPageComponent/AuthenticationPageHeader'
-
-import AuthenticationPageHeader from '../pageComponents/AuthenticationPageComponent/AuthenticationPageHeader'
 
 
 const AuthenticanPage = () => {
