da63d114e3368144fa1cfc14f559cc326a71f36c
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2023-10-02 10:04:42 +0530

    dfksbdfkb

--- client/src/pages/AuthenticanPage.js
+++ client/src/pages/AuthenticanPage.js
@@ -2,9 +2,12 @@ import React from 'react'
 import SignIn from '../pageComponents/AuthenticationPageComponent/SignIn'
 import AuthenticanPageHeader from '../pageComponents/AuthenticationPageComponent/AuthenticationPageHeader'
 
+import AuthenticationPageHeader from '../pageComponents/AuthenticationPageComponent/AuthenticationPageHeader'
+
+
 const AuthenticanPage = () => {
   return (
-    // <SignIn/>
+
     <AuthenticanPageHeader/>
   )
 }
