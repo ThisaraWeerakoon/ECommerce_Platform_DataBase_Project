ab32cce585a77afe7a4979d3df2b05d71085a1aa
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2023-10-27 07:07:08 +0530

    Add the Filtering Section

--- SQL/Filter/product.sql
+++ SQL/Filter/product.sql
@@ -1,23 +1,19 @@
--- DROP VIEW All_product_In_Store;
-
--- CREATE VIEW All_product_In_Store AS
-
-SELECT 
-	v.SKU,
-	p.Name AS product, 
-	pc.Category_Name,
-    vt.Variation_Name,
-    vo.Variation_Option_Name,
+SELECT
+    v.SKU,
+    p.Name AS product,
+    pc.Category_Name,
     v.Weight,
     v.Quantity
-FROM product p
+FROM
+    product p
 INNER JOIN product_category_configuration pcc ON p.Product_Id = pcc.Product_Id
 INNER JOIN product_category pc ON pcc.Product_Category_Id = pc.Product_Category_Id
 INNER JOIN variant_type vt ON pc.Product_Category_Id = vt.Product_Category_Id
 INNER JOIN variation_option vo ON vt.Variant_Type_Id = vo.Variant_Type_Id
 INNER JOIN variation_configuration vc ON vo.Variation_Option_Id = vc.Variation_Option_Id
 INNER JOIN variant v ON vc.Variant_Id = v.Variant_Id
--- WHERE p.name = "Samsung S21";
-
--- I comment First 2 code because it will get error
--- But there are in the code
+-- WHERE
+--     p.name = "Samsung S21"
+GROUP BY
+    p.Name, pc.Category_Name, v.SKU, v.Weight, v.Quantity
+LIMIT 0, 1000;
