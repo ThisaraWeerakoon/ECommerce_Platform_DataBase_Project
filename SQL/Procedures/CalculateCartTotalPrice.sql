11530d4ee975feb9d16cf96fbc34f3966ae8e453
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2023-10-27 06:43:02 +0530

    Update User Report

--- SQL/Procedures/CalculateCartTotalPrice.sql
+++ SQL/Procedures/CalculateCartTotalPrice.sql
@@ -16,4 +16,12 @@ BEGIN
     SET totalPrice = cartTotal;
 END //
 
+DELIMITER //
+
+CREATE PROCEDURE GenerateUserReport()
+BEGIN
+    SELECT user_id, first_name, last_name, email
+    FROM users;
+END //
+
 DELIMITER ;
