5d9bdafadca83f43d2f12b379ce6048187abc4cc
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2023-10-27 10:21:25 +0530

    Update User address is Default

--- SQL/Procedures/UserReport.sql
+++ SQL/Procedures/UserReport.sql
@@ -5,7 +5,9 @@ DROP PROCEDURE IF EXISTS GenerateUserReport;
 CREATE PROCEDURE GenerateUserReport()
 BEGIN
     SELECT user_id, first_name, last_name, email, User_Type
-	FROM user;
+	FROM user u;
+    JOIN variant v ON oi.Variant_Id = v. Variant_Id
+	  JOIN product p ON v.Product_Id = p. Product_Id;
 END //
 
 DELIMITER ;
--- SQL/Tables/update.sql
+++ SQL/Tables/update.sql
@@ -2,3 +2,11 @@ UPDATE variant
 SET SKU = 1002,
 	Weight = 160
 WHERE variant_Id = 2
+
+
+-- Update user address Is_default to 1
+UPDATE user_address SET Is_Default = 1 
+WHERE user_id IN (1, 2, 3, 4);
+
+UPDATE user_address SET Is_Default = 0
+WHERE Address_Id = 2
