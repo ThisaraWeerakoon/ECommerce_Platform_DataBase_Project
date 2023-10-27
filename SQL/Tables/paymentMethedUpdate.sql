871b312b28302be102437370df4938ae719a37a0
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2023-10-27 18:16:02 +0530

    Payment Method Update

---
+++ SQL/Tables/paymentMethedUpdate.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_payment_method (Payment_method_id, User_Id, Payment_type, Provider, Account_Number, Expiry_Date, Is_Default)
+Values (1, 1, 'Card', 'San', 0773756089,'2023-10-27',1)
