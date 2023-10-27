2d114b76a46dc57ae395b0f20ba6ca82e95661fc
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2023-10-27 09:20:57 +0530

    Fix User Report

--- SQL/Procedures/UserReport.sql
+++ SQL/Procedures/UserReport.sql
@@ -1,9 +1,11 @@
 DELIMITER //
 
+DROP PROCEDURE IF EXISTS GenerateUserReport;
+
 CREATE PROCEDURE GenerateUserReport()
 BEGIN
-    SELECT user_id, first_name, last_name, email
-    FROM users;
+    SELECT user_id, first_name, last_name, email, User_Type
+	FROM user;
 END //
 
 DELIMITER ;
