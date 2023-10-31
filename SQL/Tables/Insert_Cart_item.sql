fb72a8b68557a212e315ce348dae4728dc51a40d
ISKumarasingha <sanjanakumarasingha1@gmail.com>
2023-10-31 15:44:25 +0530

    arrange

--- SQL/Tables/Insert_Cart_item.sql
+++ SQL/Tables/Insert_Cart_item.sql
@@ -1,9 +1,9 @@
 
 INSERT INTO cart_item (Cart_Id, Variant_Id, cart_item_quantity)
-Values (1,1,12),
-	(1,3,3),
-    (1,4,27),
-    (2,5,16)
+Values  (1,1,12),
+        (1,3,3),
+        (1,4,27),
+        (2,5,16)
 
 
     
